23b00cb75109e9b63c7ac76fde89f21b0f8e4edc
Anuj Shankar <anujshankar95@gmail.com>
2017-01-19 15:06:25 +0530

    20

--- for-loop.js
+++ for-loop.js
@@ -1,6 +1,6 @@
-var total = 0;
+var sumOfNumbers = 0;
 var limit = 10;
 for( var i = 0; i < limit; i++){
-    total += i;
+    sumOfNumbers += i;
 }
-console.log(total);
+console.log(sumOfNumbers);
---
+++ function-arguments.js
@@ -0,0 +1,4 @@
+function multiplyThreeNumbers(number1,number2,number3){
+    return (number1+(number2*number3));
+}
+console.log(multiplyThreeNumbers(53,61,67));
